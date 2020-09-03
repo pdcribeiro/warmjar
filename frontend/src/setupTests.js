6fc1a332b65ed3da56bb985deffe587f7cf1d03f
pdcribeiro <pdcribeiro95@gmail.com>
2020-09-03 17:25:39 +0100

    test: add Login test

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+import { useCSRFToken } from './hooks/use-csrf-token';
+
+jest.mock('./hooks/use-csrf-token');
+
+useCSRFToken.mockReturnValue('csrftoken');
